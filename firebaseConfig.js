5d89237d66d6c8d5a34753be628486912fa1216d
Nipuna Kaweya <124517075+Nipun23a@users.noreply.github.com>
2024-08-08 09:13:16 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9Wm-l8-EirURTCF5eyDv_vvVikitzf7U",
-    authDomain: "medi-help-app-c7a08.firebaseapp.com",
-    projectId: "medi-help-app-c7a08",
-    storageBucket: "medi-help-app-c7a08.appspot.com",
-    messagingSenderId: "589690549554",
-    appId: "1:589690549554:web:dddfd4b138cc7083cb132b",
-    measurementId: "G-8TGFWHY6JP"
+ #firebase configuration
 };
 
 const app = initializeApp(firebaseConfig);
